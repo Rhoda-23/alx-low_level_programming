ee3c6c528d58462a83d59d9045422468d168e241
RHODA <rhodaazuatalam@gmail.com>
2022-10-07 15:17:33 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,39 +1,19 @@
 #include "main.h"
 /**
-*create_array - array for prints a string
-*@size: number elements array
-*@c: char
-*Return: pointer
+*malloc_checked - array for prints a string
+*@b: number of memory
+*Return: void
 */
 
-char *create_array(unsigned int size, char c)
+void *malloc_checked(unsigned int b)
 {
-char *buffer;
-unsigned int position;
+void *p;
 
-if (size == 0)
+p = malloc(b);
+if (p == NULL)
 {
-return (NULL);
+exit(98);
 }
 
-/*Define values with malloc*/
-buffer = (char *) malloc(size * sizeof(c));
-
-if (buffer == 0)
-{
-return (NULL);
-}
-
-else
-{
-osition = 0;
-while (position < size) /*While for array*/
-{
-*(buffer + position) = c;
-position++;
-}
-
-return (buffer);
-}
-
+return (p);
 }
